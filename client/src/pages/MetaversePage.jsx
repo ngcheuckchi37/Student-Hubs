f0729fee15806c078874c04dfcc5109504757cad
ngcheuckchi37 <116089900+ngcheuckchi37@users.noreply.github.com>
2023-05-16 00:46:10 +0800

    Update MetaversePage.jsx

--- client/src/pages/MetaversePage.jsx
+++ client/src/pages/MetaversePage.jsx
@@ -1,40 +1,10 @@
 import React from 'react'
 
-
-
-
-
-
-export default function MetaversePage() {
+export const MetaversePage = () => {
   return (
-    <>
-      {
-        
-        
-        <p><a href="https://www.w3schools.com" target="iframe_a" text-blue-600 >W3Schools.com</a></p>
-
-
-}
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              <a href="http://127.0.0.1:5173/postpage">go home</a>
-            </a>
-            <a href="#" className="text-sm font-semibold text-gray-900 ">
-              Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
-        </div>
-      </main>
-    </>
+  <div>
+  <iframe src="http://127.0.0.1:8887/" width="100%" height="700px"></iframe>
+  </div>  
   )
-
-
 }
+export default MetaversePage;
